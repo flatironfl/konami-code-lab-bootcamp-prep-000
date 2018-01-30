88424cdf496802d67821858de6af3ead0c35cb96
First Last <flatironcamp.tc99@mamber.net>
2018-01-30 01:07:44 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -4,7 +4,7 @@ describe('index', () => {
   function triggerKeyDown(which) {
     const keyboardEvent = document.createEvent("KeyboardEvent")
 
-    kbdEvent.initKeyboardEvent(
+    keyboardEvent.initKeyboardEvent(
       'keydown',
       true,
       true,
